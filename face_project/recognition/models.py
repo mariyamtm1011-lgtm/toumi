52a8335797b8081c776e6347330aa2f7f7df6e19
boussilayasmine <boussilayassmine@gmail.com>
2025-12-06 17:52:04 +0100

    modificaion

--- face_project/recognition/models.py
+++ face_project/recognition/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='faces/', blank=True, null=True)   # media/faces/
+    role = models.CharField(max_length=20, default='user')
+
+    def __str__(self):
+        return self.user.username
+
+class AccessLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
+    result = models.CharField(max_length=20)  # 'granted' / 'denied' / 'unknown'
+    note = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.date_time} - {self.user} - {self.result}"
